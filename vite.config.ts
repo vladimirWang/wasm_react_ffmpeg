07958fa8c32c7f8335ab70e97f185f0a3afe3cff
fernando <413114463@qq.com>
2026-01-09 14:28:50 +0800

    开启react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
     tailwindcss()
   ],
   server: {
