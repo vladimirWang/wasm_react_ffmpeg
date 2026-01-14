38d0eb71df3209319316a175195578432163ab9e
fernando <413114463@qq.com>
2026-01-14 17:02:16 +0800

    加依赖，配置转发proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,17 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        // rewrite(path): string {
+        //   return path.replace(/^\/api/, '')
+        // },
+      },
+    },
+  },
   plugins: [
     react({
       babel: {
