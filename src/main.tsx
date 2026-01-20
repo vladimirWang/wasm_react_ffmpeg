77f815fd42ae55af4492b74e411b6d044f7240c1
fernando <413114463@qq.com>
2026-01-20 17:41:04 +0800

    dayjs扩展插件

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { ModalComponent } from "./components/GlobalModal.tsx";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+// 注册插件
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+// 设置时区为上海
+dayjs.tz.setDefault("Asia/Shanghai");
 // import { mock, result } from './mazeUtils/1.ts'
 
 // console.log('isconnected: ', mock, result);
