da964b4088bd34a44f21d977057054318a554c15
fernando <413114463@qq.com>
2026-01-26 17:42:18 +0800

    热销商品时间取值按实际的来

--- src/pages/Home/HotSales.tsx
+++ src/pages/Home/HotSales.tsx
@@ -2,6 +2,7 @@ import { Pie } from "@ant-design/charts";
 import React, { useEffect, useState } from "react";
 import { getHotSales } from "../../api/statistics";
 import { message } from "antd";
+import dayjs from "dayjs";
 
 interface HotSalesData {
 	type: string;
@@ -11,11 +12,13 @@ interface HotSalesData {
 const HotSales = () => {
 	const [data, setData] = useState<HotSalesData[]>([]);
 
+	const startDate = dayjs().subtract(3, "month").toDate();
+	const endDate = dayjs().toDate();
 	const loadData = async () => {
 		try {
-			// 2025-06-01&endDate=2026-01-26
-			const startDate = new Date("2025-08-01");
-			const endDate = new Date("2026-01-26");
+			// // 2025-06-01&endDate=2026-01-26
+			// const startDate = new Date("2025-08-01");
+			// const endDate = new Date("2026-01-26");
 			const result = await getHotSales({ startDate, endDate });
 			// 根据 API 返回的数据格式更新状态
 			// 假设 API 返回的数据格式为 { type: string, value: number }[]
@@ -40,6 +43,10 @@ const HotSales = () => {
 		data,
 		angleField: "value",
 		colorField: "type",
+		// color: ["#FF6B6B", "#4ECDC4", "#FFD166", "#06D6A0"],
+		title: {
+			title: `${dayjs(startDate).format("YYYY-MM-DD")} 至 ${dayjs(endDate).format("YYYY-MM-DD")} 热销商品`,
+		},
 		label: {
 			text: "value",
 			style: {
