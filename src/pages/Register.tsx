e5d41708c95d28f31f948aa057a4add99cf06869
fernando <413114463@qq.com>
2026-02-05 20:30:05 +0800

    注册页加confetti

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import type { FormItemProps, FormProps } from "antd";
 import { Button, Card, Checkbox, Form, Input, Space, Steps } from "antd";
 import {
@@ -9,6 +9,9 @@ import {
 	checkEmailVerificationCode,
 } from "../api/user";
 import { Link, useNavigate } from "react-router-dom";
+import { useWindowSize } from "react-use";
+import Confetti from "react-confetti";
+import { sleep } from "../utils/common";
 
 /** 垂直布局：标签与输入均占满一行，避免右侧留白 */
 const formItemLayout: FormProps = {
@@ -48,11 +51,13 @@ const Register: React.FC = () => {
 	const [countdown, setCountdown] = useState(0);
 	const [step1Loading, setStep1Loading] = useState(false);
 	const [step2Loading, setStep2Loading] = useState(false);
+	const { width, height } = useWindowSize();
+	const [confettiVisible, setConfettiVisible] = useState(false);
+
 	const sendVerifyCode = async () => {
 		try {
 			await form.validateFields(["email"]);
 			const email = form.getFieldValue("email");
-			// TODO: 调用发送验证码接口，例如 sendEmailVerifyCode(email)
 			await sendEmailVerificationCode(email);
 			console.log("发送验证码到:", email);
 			setCountdown(60);
@@ -95,6 +100,9 @@ const Register: React.FC = () => {
 			const { password, username } = values;
 			const res: RegisterResponse = await userRegister({ email, password, username });
 			console.log("注册结果:", res);
+			setConfettiVisible(true);
+			await sleep(4500);
+			setConfettiVisible(false);
 			if (res.code === 200) {
 				navigate("/landing/login");
 			}
@@ -107,122 +115,125 @@ const Register: React.FC = () => {
 	};
 
 	return (
-		<Card style={{ width: 500 }}>
-			<div className="flex flex-col items-center">
-				<Steps current={currentStep} items={stepItems} style={{ marginBottom: 24 }} />
-				<Form
-					style={{ width: "100%" }}
-					{...formItemLayout}
-					form={form}
-					name="register"
-					onFinish={onFinish}
-					initialValues={registerFormInitialValues}
-					scrollToFirstError
-					layout="vertical"
-				>
-					{currentStep === 0 && (
-						<>
-							<Form.Item
-								name="email"
-								label="邮箱"
-								rules={[
-									{ type: "email", message: "请输入有效的邮箱地址！" },
-									{ required: true, message: "请输入邮箱！" },
-								]}
-							>
-								<Input placeholder="请输入邮箱" />
-							</Form.Item>
-							<Form.Item
-								name="verifyCode"
-								label="验证码"
-								rules={[{ required: true, message: "请输入验证码！" }]}
-							>
-								<Space.Compact style={{ width: "100%" }}>
-									<Input placeholder="请输入验证码" />
-									<Button type="primary" onClick={sendVerifyCode} disabled={countdown > 0}>
-										{countdown > 0 ? `${countdown}秒后重发` : "发送验证码"}
+		<div>
+			<Card style={{ width: 500 }}>
+				<div className="flex flex-col items-center">
+					<Steps current={currentStep} items={stepItems} style={{ marginBottom: 24 }} />
+					<Form
+						style={{ width: "100%" }}
+						{...formItemLayout}
+						form={form}
+						name="register"
+						onFinish={onFinish}
+						initialValues={registerFormInitialValues}
+						scrollToFirstError
+						layout="vertical"
+					>
+						{currentStep === 0 && (
+							<>
+								<Form.Item
+									name="email"
+									label="邮箱"
+									rules={[
+										{ type: "email", message: "请输入有效的邮箱地址！" },
+										{ required: true, message: "请输入邮箱！" },
+									]}
+								>
+									<Input placeholder="请输入邮箱" />
+								</Form.Item>
+								<Form.Item
+									name="verifyCode"
+									label="验证码"
+									rules={[{ required: true, message: "请输入验证码！" }]}
+								>
+									<Space.Compact style={{ width: "100%" }}>
+										<Input placeholder="请输入验证码" />
+										<Button type="primary" onClick={sendVerifyCode} disabled={countdown > 0}>
+											{countdown > 0 ? `${countdown}秒后重发` : "发送验证码"}
+										</Button>
+									</Space.Compact>
+								</Form.Item>
+								<Form.Item {...tailFormItemLayout}>
+									<Button block type="primary" onClick={onNextStep} loading={step1Loading}>
+										下一步
 									</Button>
-								</Space.Compact>
-							</Form.Item>
-							<Form.Item {...tailFormItemLayout}>
-								<Button block type="primary" onClick={onNextStep} loading={step1Loading}>
-									下一步
-								</Button>
-							</Form.Item>
-						</>
-					)}
-					{currentStep === 1 && (
-						<>
-							<Form.Item
-								name="username"
-								label="用户名"
-								rules={[
-									{ required: true, message: "请输入用户名！" },
-									{ max: 8, message: "用户名长度不能大于8位" },
-								]}
-							>
-								<Input placeholder="请输入用户名" />
-							</Form.Item>
-							<Form.Item
-								name="password"
-								label="密码"
-								rules={[
-									{ required: true, message: "请输入密码！" },
-									{ min: 6, message: "密码长度不能小于6位" },
-									{ max: 8, message: "密码长度不能大于8位" },
-								]}
-								hasFeedback
-							>
-								<Input.Password placeholder="请输入密码" />
-							</Form.Item>
-							<Form.Item
-								name="confirm"
-								label="确认密码"
-								dependencies={["password"]}
-								hasFeedback
-								rules={[
-									{ required: true, message: "请确认密码！" },
-									({ getFieldValue }) => ({
-										validator(_, value) {
-											if (!value || getFieldValue("password") === value) {
-												return Promise.resolve();
-											}
-											return Promise.reject(new Error("两次输入的密码不一致！"));
+								</Form.Item>
+							</>
+						)}
+						{currentStep === 1 && (
+							<>
+								<Form.Item
+									name="username"
+									label="用户名"
+									rules={[
+										{ required: true, message: "请输入用户名！" },
+										{ max: 8, message: "用户名长度不能大于8位" },
+									]}
+								>
+									<Input placeholder="请输入用户名" />
+								</Form.Item>
+								<Form.Item
+									name="password"
+									label="密码"
+									rules={[
+										{ required: true, message: "请输入密码！" },
+										{ min: 6, message: "密码长度不能小于6位" },
+										{ max: 8, message: "密码长度不能大于8位" },
+									]}
+									hasFeedback
+								>
+									<Input.Password placeholder="请输入密码" />
+								</Form.Item>
+								<Form.Item
+									name="confirm"
+									label="确认密码"
+									dependencies={["password"]}
+									hasFeedback
+									rules={[
+										{ required: true, message: "请确认密码！" },
+										({ getFieldValue }) => ({
+											validator(_, value) {
+												if (!value || getFieldValue("password") === value) {
+													return Promise.resolve();
+												}
+												return Promise.reject(new Error("两次输入的密码不一致！"));
+											},
+										}),
+									]}
+								>
+									<Input.Password placeholder="请再次输入密码" />
+								</Form.Item>
+								<Form.Item
+									name="agreement"
+									valuePropName="checked"
+									rules={[
+										{
+											validator: (_, value) =>
+												value ? Promise.resolve() : Promise.reject(new Error("请阅读并同意协议")),
 										},
-									}),
-								]}
-							>
-								<Input.Password placeholder="请再次输入密码" />
-							</Form.Item>
-							<Form.Item
-								name="agreement"
-								valuePropName="checked"
-								rules={[
-									{
-										validator: (_, value) =>
-											value ? Promise.resolve() : Promise.reject(new Error("请阅读并同意协议")),
-									},
-								]}
-								{...tailFormItemLayout}
-							>
-								<Checkbox>
-									我已阅读 <a href="">用户协议</a>
-								</Checkbox>
-							</Form.Item>
-							<Form.Item {...tailFormItemLayout}>
-								<Button block type="primary" htmlType="submit" loading={step2Loading}>
-									注册
-								</Button>
-								<div className="flex justify-center items-center gap-4 mt-4">
-									<Button onClick={onPrevStep}>上一步</Button>
-									<Link to="/landing/login">已有账号？去登录</Link>
-								</div>
-							</Form.Item>
-						</>
-					)}
-				</Form>
-			</div>
-		</Card>
+									]}
+									{...tailFormItemLayout}
+								>
+									<Checkbox>
+										我已阅读 <a href="">用户协议</a>
+									</Checkbox>
+								</Form.Item>
+								<Form.Item {...tailFormItemLayout}>
+									<Button block type="primary" htmlType="submit" loading={step2Loading}>
+										注册
+									</Button>
+									<div className="flex justify-center items-center gap-4 mt-4">
+										<Button onClick={onPrevStep}>上一步</Button>
+										<Link to="/landing/login">已有账号？去登录</Link>
+									</div>
+								</Form.Item>
+							</>
+						)}
+					</Form>
+				</div>
+			</Card>
+			{confettiVisible && <Confetti width={width} height={height} />}
+		</div>
 	);
 };
 
