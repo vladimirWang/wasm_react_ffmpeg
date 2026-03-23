0ee3cf6071f01be94f26937a376b504cc9f06b44
fernando <413114463@qq.com>
2026-03-23 12:42:47 +0800

    公共图片验证码组件

---
+++ src/components/Captcha.tsx
@@ -0,0 +1,61 @@
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from "react";
+import { getCaptcha } from "../api/user";
+import { useLocation } from "react-router-dom";
+
+export type CaptchaHandle = {
+	refreshCaptcha: () => void;
+};
+
+interface CaptchaProps {
+	onChange: (captchaId: string) => void;
+	pathname: string;
+	pathnamesRefresh: string[];
+}
+
+function Captcha(
+	{ onChange, pathname, pathnamesRefresh }: CaptchaProps,
+	ref: React.Ref<CaptchaHandle>
+): React.ReactElement {
+	const [captchaSrc, setCaptchaSrc] = useState<string>();
+
+	useImperativeHandle(ref, () => ({
+		refreshCaptcha: loadCaptcha,
+	}));
+
+	const loadCaptcha = async () => {
+		const src = await getCaptcha();
+		setCaptchaSrc(src.image);
+		// setCaptchaId(src.captchaId);
+		onChange(src.captchaId);
+	};
+
+	useEffect(() => {
+		if (!pathnamesRefresh.includes(pathname)) {
+			loadCaptcha();
+		}
+	}, [pathname]);
+
+	useEffect(() => {
+		function loadCaptchaWhenVisible() {
+			if (document.visibilityState !== "visible") return;
+			loadCaptcha();
+		}
+		document.addEventListener("visibilitychange", loadCaptchaWhenVisible, false);
+		return () => {
+			document.removeEventListener("visibilitychange", loadCaptchaWhenVisible, false);
+			// window.removeEventListener("load", loadCaptcha, false);
+		};
+	}, []);
+	return (
+		<img
+			src={captchaSrc}
+			width={100}
+			height={40}
+			alt="验证码"
+			onClick={loadCaptcha}
+			style={{ background: "white" }}
+		/>
+	);
+}
+
+export default forwardRef<CaptchaHandle, CaptchaProps>(Captcha);
